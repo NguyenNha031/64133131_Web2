6f6c0acb8b91417be4901c57d5baa16a902060c1
NguyenNha031 <nha.nt.64cntt@ntu.edu.vn>
2025-03-25 09:43:24 +0700

    Bo sung bai TongHopGK

--- TongHopGK/src/main/java/vn/edu/nha/tonghopgk/controllers/HomeController.java
+++ TongHopGK/src/main/java/vn/edu/nha/tonghopgk/controllers/HomeController.java
@@ -1,23 +1,38 @@
 package vn.edu.nha.tonghopgk.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
+    List<SinhVien> dsSV = new ArrayList<>();
+
+    public HomeController() {
+        dsSV.add(new SinhVien("SV001", "Nguyễn Văn A", 8.5));
+        dsSV.add(new SinhVien("SV002", "Trần Thị B", 7.0));
+        dsSV.add(new SinhVien("SV003", "Lê Văn C", 6.8));
+    }
+    @GetMapping("/studentList")
+    public String listPage(Model model) {
+        model.addAttribute("dsSV", dsSV);
+        return "studentList";
+    }
+
     @GetMapping("/homepage")
     public String home() {
         return "index";
     }
+
     @GetMapping("/about")
     public String aboutPage() {
         return "about";
     }
 
-    @GetMapping("/list")
-    public String listPage() {
-        return "list";
-    }
+
 
     @GetMapping("/address")
     public String addressPage() {
